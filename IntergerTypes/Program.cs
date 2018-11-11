e29ddcdbf3ce263ae9b45f1223bca2c1893cafdc
Bach Ngoc Toan <ngoctoan@live.com>
2018-11-11 16:05:36 +0700

    Add project files.

---
+++ IntergerTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntergerTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte centuries = 20;
+            ushort years = 2000;
+            uint days = 730480;
+            ulong hours = 17531530;
+
+            Console.WriteLine(centuries + " centuries are " + years + " years, or " + days + " days, or " + hours + " hours.");
+
+            ulong maxIntValue = ulong.MaxValue;
+            Console.WriteLine(maxIntValue);
+            Console.Read();
+        }
+    }
+}
---
+++ RealFloatingPoint/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealFloatingPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //float floatPI = 3.14f;
+            //Console.WriteLine(floatPI); // 3.14 
+            //double doublePI = 3.14;
+            //Console.WriteLine(doublePI); // 3.14 
+            //double nan = Double.NaN;
+            //Console.WriteLine(nan); // NaN 
+            //double infinity = Double.PositiveInfinity;
+            //Console.WriteLine(infinity); // Infinity
+
+            float floatPI = 3.141592653589793238f;
+            double doublePI = 3.141592653589793238;
+            // Print the results on the console 
+            Console.WriteLine("Float PI is: " + floatPI);
+            Console.WriteLine("Double PI is: " + doublePI);
+
+            Console.Read();
+        }
+    }
+}
