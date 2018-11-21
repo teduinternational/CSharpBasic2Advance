ab14fc7bdce41fc867c5c68c5a7c8753c886c7d3
Bach Ngoc Toan <ngoctoan@live.com>
2018-11-21 07:51:58 +0700

    String, object and nullable

---
+++ NullableType/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            int? ni = 6;
+            //Nullable<int> i = 5;
+            Console.WriteLine(ni.HasValue);
+            Console.WriteLine(ni.HasValue ? ni.Value : 0);
+            Console.WriteLine(ni.GetValueOrDefault());
+
+            Console.Read();
+        }
+    }
+}
---
+++ ObjectType/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object container1 = 5;
+            object container2 = "Five";
+            object container3 = true;
+
+            Console.WriteLine("Container 1: " + container1);
+
+            Console.WriteLine("Container 2: " + container2);
+
+            Console.Read();
+
+        }
+    }
+}
