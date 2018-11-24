24aa6b0500eb94881711d5130fd4df5423558034
Bach Ngoc Toan <ngoctoan@live.com>
2018-11-24 15:49:19 +0700

    14, 15 ,16

---
+++ Literals/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Literals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var character = '\\';
+            Console.WriteLine(character);
+            Console.Read();
+        }
+    }
+}
---
+++ Variables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte centuries = 20;
+            ushort years = 2000;
+
+            decimal decimalPI = 3.141592m;
+
+            bool isEmpty = true;
+            char ch = 'a';
+
+            string firstName = "John";
+
+            ch = (char)5;
+            char secondChar;
+            secondChar = ch;
+            Console.Read();
+        }
+    }
+}
